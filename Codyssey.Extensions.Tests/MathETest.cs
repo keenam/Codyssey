2339e0c117bb9e5271ef6e376b96e91759b6eadd
Kee Nam <kee.nam@outlook.com>
2019-12-22 16:31:33 -0800

    MathE(xtension) added to support Floor double value with fixed decimal points.

---
+++ Codyssey.Extensions.Tests/MathETest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xunit;
+
+namespace Codyssey.Extensions.Tests
+{
+    /// <summary>
+    /// Tests <see cref="MathE"/>
+    /// </summary>
+    public class MathETest
+    {
+        [Theory]
+        [InlineData(1 / 3.0, 2, 0.33)]
+        [InlineData(1 / 3.0, 9, 0.333_333_333)]
+        [InlineData(2.145, 2, 2.14)]
+        public void TestGetFixedPoint(double value, int fixedPoint, double expected)
+        {
+            Assert.Equal(expected, MathE.Floor(value, fixedPoint));
+        }
+
+        [Fact]
+        public void TestGetFixedPointDecimalException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                MathE.Floor(1.23, -1);
+            });
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                MathE.Floor(1.23, 10);
+            });
+        }
+    }
+}
---
+++ Codyssey.Extensions/MathE.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codyssey.Extensions
+{
+    /// <summary>
+    /// Math extensions
+    /// </summary>
+    public static class MathE
+    {
+        /// <summary>
+        /// Returns the value limited to the decimal count for comparison.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="decimalCount"></param>
+        /// <returns>Fixed point value</returns>
+        /// <remarks>Use this to retrieve comparable values</remarks>
+        public static double Floor(double value, int decimalCount)
+        {
+            if (decimalCount < 0 || decimalCount > 9)
+            {
+                throw new ArgumentException("Limit the decimal count to beteen 0 and 9", nameof(decimalCount));
+            }
+            var epsilon = Math.Pow(10, -decimalCount);
+            return ((long)(value / epsilon)) * epsilon;
+        }
+    }
+}
