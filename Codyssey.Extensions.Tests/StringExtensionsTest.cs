d77e3e00020f2a79d0cd844cba28e03cb372ce6e
Kee Nam <kee.nam@outlook.com>
2020-05-24 16:23:01 -0700

    added StringExtensions
    - SplitByLength
    - GetCharacters

---
+++ Codyssey.Extensions.Tests/StringExtensionsTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Codyssey.Extensions.Tests
+{
+    /// <summary>
+    /// Tests <see cref="StringExtensions"/>
+    /// </summary>
+    public class StringExtensionsTest
+    {
+        [Fact]
+        public void TestSplitByLength()
+        {
+            var split = new string('0', 3).SplitByLength(2);
+            Assert.Equal(2, split.Count());
+            Assert.True(split.First().SequenceEqual(new char[] { '0', '0' }));
+            Assert.True(split.Skip(1).Take(1).First().SequenceEqual(new char[] { '0' }));
+        }
+    }
+}
---
+++ Codyssey.Extensions/StringExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codyssey.Extensions
+{
+    /// <summary>
+    /// StringExtensions
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Efficiently splits the text in <c>char</c> enumerable by length.
+        /// </summary>
+        /// <param name="text">Text to retrieve char enumerable of <paramref name="length"/></param>
+        /// <param name="length">Length to split by</param>
+        /// <returns>A sequence of char enumerable that has upto the length of the character</returns>
+        public static IEnumerable<IEnumerable<char>> SplitByLength(this string text, int length)
+        {
+            var max = text.Length;
+            for (int start = 0; start < max; start += length)
+            {
+                yield return GetCharacters(text, start, Math.Min(start + length, max));
+            }
+        }
+
+        /// <summary>
+        /// Gets characters between start index and end - 1.
+        /// </summary>
+        /// <param name="source">String source to return characters</param>
+        /// <param name="start">Start index</param>
+        /// <param name="end">End index -1 to retrieve</param>
+        public static IEnumerable<char> GetCharacters(this string source, int start, int end)
+        {
+            for (var index = start; index < end; ++index)
+            {
+                yield return source[index];
+            }
+        }
+    }
+}
