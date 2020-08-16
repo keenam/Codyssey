3be8fd55b88dbaa42ef32959ae31056ef8703010
Kee Nam <kee.nam@outlook.com>
2020-08-15 20:44:44 -0700

    Added FileExtensions to read 4KBs blocks

---
+++ Codyssey.Extensions.Tests/FileExtensionsTest.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Codyssey.Extensions.Tests
+{
+    /// <summary>
+    /// Tests <see cref="FileExtensions"/>
+    /// </summary>
+    public class FileExtensionsTest
+    {
+        [Theory]
+        [InlineData(0, new int[] { })]
+        [InlineData(4096, new int[] { 4096 })]
+        [InlineData(4097, new int[] { 4096, 1 })]
+        public void TestRead4KBBlocks(int size, int[] blockSizes)
+        {
+            var filePath = Path.GetTempFileName();
+            File.WriteAllBytes(filePath, Encoding.UTF8.GetBytes(new string('a', size)));
+
+            Assert.Equal(blockSizes, filePath.Read4KBBlocks().Select(block => block.Length));
+        }
+    }
+}
---
+++ Codyssey.Extensions/FileExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codyssey.Extensions
+{
+    /// <summary>
+    /// File utilities.
+    /// </summary>
+    public static class FileExtensions
+    {
+        /// <summary>
+        /// Gets 4KB blocks of the file
+        /// </summary>
+        /// <param name="filePath">The file path to read file from</param>
+        /// <returns>4KB or less data in sequence</returns>
+        /// <remarks>
+        /// The file is locked until it's completely read or read is stopped.
+        /// </remarks>
+        public static IEnumerable<byte[]> Read4KBBlocks(this string filePath)
+        {
+            const int bufferSize_4KB = 4 * 1024;
+            using (var fileStream = File.OpenRead(filePath))
+            {
+                var buffer = new byte[bufferSize_4KB];
+
+                while (true)
+                {
+                    var bytesRead = fileStream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+
+                    if (bytesRead == buffer.Length)
+                    {
+                        yield return buffer;
+                    }
+                    else
+                    {
+                        yield return buffer.Take(bytesRead).ToArray();
+                    }
+                }
+            }
+        }
+    }
+}
