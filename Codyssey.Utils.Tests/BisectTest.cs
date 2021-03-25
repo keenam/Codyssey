8178c3ae9c4f7a1419c3b417673b5aec0107c088
Kee D Nam <kee.nam@outlook.com>
2021-03-25 00:40:51 -0700

    Added Bisect to Codyssey.Utils.Search
    (also upgraded to .NET 5)

---
+++ Codyssey.Utils.Tests/BisectTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Codyssey.Utils.Search;
+using Xunit;
+
+namespace Codyssey.Utils.Tests
+{
+    /// <summary>
+    /// Test Bisect
+    /// </summary>
+    public partial class BisectTest
+    {
+        [Fact]
+        public void TestBisect()
+        {
+            var haystack = new int[] { 1, 4, 5, 6, 8, 12, 15, 20, 21, 23, 23, 26, 29, 30 };
+            var needle = new int[] { 0, 1, 2, 5, 8, 10, 22, 23, 29, 30, 31 };
+            var rightExpected = new int[] { 0, 1, 1, 3, 5, 5, 9, 11, 13, 14, 14 };
+            var leftExpected = new int[] { 0, 0, 1, 2, 4, 5, 9, 9, 12, 13, 14 };
+
+            // items provided to the Bisect object is sorted order
+            // from there, it will find item left of 8
+
+            var bisect = new Bisect<int>(haystack, Comparer<int>.Default);
+
+            Assert.Equal(rightExpected, needle.Select(bisect.Right).ToArray());
+            Assert.Equal(leftExpected, needle.Select(bisect.Left).ToArray());
+        }
+    }
+}
---
+++ Codyssey.Utils/Search/Bisect.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace Codyssey.Utils.Search
+{
+    /// <summary>
+    /// A Bisect utility on a sorted items to find the insertion point.
+    /// </summary>
+    /// <remarks>
+    /// Call <see cref="Right"/> to return an insertion point after existing item.
+    /// Call <see cref="Left"/> to return the position of the existing item (hence replacing it)
+    /// </remarks>
+    public class Bisect<T>
+    {
+        private readonly T[] items;
+        private readonly IComparer<T> comparer;
+        private readonly int median;
+
+        public Bisect(T[] items, IComparer<T> comparer)
+        {
+            this.items = items;
+            this.comparer = comparer;
+            this.median = items.Length / 2;
+        }
+
+        public int Left(T value)
+        {
+            return this.GetInsertIndex(value, 0, this.items.Length - 1, this.median, InsertPosition.Before);
+        }
+
+        /// <summary>
+        /// Gets the right index of the value.
+        /// </summary>
+        /// <param name="value">The value to compare</param>
+        /// <returns>The insertion point after an existing item. 0 if it's the first.</returns>
+        public int Right(T value)
+        {
+            return this.GetInsertIndex(value, 0, this.items.Length - 1, this.median, InsertPosition.After);
+        }
+
+        private int GetInsertIndex(T value, int left, int right, int compareIndex, InsertPosition insertAt)
+        {
+            if (compareIndex > right)
+            {
+                // insert last
+                return right + 1;
+            }
+
+            if (compareIndex < left)
+            {
+                // insert first
+                return left;
+            }
+
+            var result = comparer.Compare(this.items[compareIndex], value);
+            if (insertAt == InsertPosition.After && result == 0 || result < 0)
+            {
+                // find the bigger number than current
+                return this.GetInsertIndex(value, compareIndex + 1, right, compareIndex + (right - compareIndex) / 2 + 1, insertAt);
+            }
+
+            // the value is less than current item
+            return this.GetInsertIndex(value, left, compareIndex - 1, compareIndex - (compareIndex - left) / 2 - 1, insertAt);
+        }
+
+        enum InsertPosition
+        {
+            Before,
+            After
+        }
+    }
+}
